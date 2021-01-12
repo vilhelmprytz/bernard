e7e96dc0010f8a4f24f68b7496cd82705281d98e
Vilhelm Prytz <vilhelm@prytznet.se>
2021-01-12 20:59:13 +0100

    Initial commit

--- bernard/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Domain
-
-
-class DomainSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Domain
-        fields = ("id", "domain")
--- bernard/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from .serializers import DomainSerializer
-from .models import Domain
-
-
-class DomainViewSet(viewsets.ModelViewSet):
-    queryset = Domain.objects.all().order_by("domain")
-    serializer_class = DomainSerializer
