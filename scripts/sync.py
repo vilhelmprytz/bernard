18af5c2094ce8ebfa23388463acbabb79aa7e10a
Vilhelm Prytz <vilhelm@prytznet.se>
2021-01-14 20:43:54 +0100

    scripts/sync: move imort to after parent folder has been added

--- scripts/sync.py
+++ scripts/sync.py
@@ -2,13 +2,13 @@
 
 import sys
 from pathlib import Path
-from dns_providers import bind9
 
 # add parent folder
 sys.path.append(str(Path(__file__).parent.parent.absolute()))
 
 from app import app  # noqa: E402
 from models import Zone, Record  # noqa: E402
+from dns_providers import bind9  # noqa: E402
 
 with app.app_context():
     bind9.sync(zones=Zone.query.all(), records=Record.query.all())
